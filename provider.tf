013afff4c35506a858dbc13d4b29dabf0b4f2217
Sonushaik99 <116141459+Sonushaik99@users.noreply.github.com>
2022-10-19 17:07:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -15,8 +15,7 @@ provider "azurerm" {
   subscription_id = "var.subscription_id"
   tenant_id = "var.tenant_id"
   client_id = "var.client_id"
-  client_secret = "var.client_secret"
-  name = "var.name"
-  location = "var.location"
+  client_secret = "var.client_secret" 
 }
+
 
