6f3b9494231c1830af7bc176cacf8d6ac60ed6a2
Sonushaik99 <116141459+Sonushaik99@users.noreply.github.com>
2022-10-19 17:47:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,13 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
-terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.23.0"
+      source  = "hashicorp/azurerm"
+      version = "~>3.10.0"
     }
   }
+
+  required_version = ">= 1.2.3"
 }
 
 provider "azurerm" {
