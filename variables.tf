d31b20743b6833520b9eebc901de33651bc259b1
Sonushaik99 <116141459+Sonushaik99@users.noreply.github.com>
2022-10-19 15:26:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,20 +8,3 @@ variable "location"{
     type = string
     default = "West Europe"
 }
-variable "Client_Id"{
-    type = string
-    default = "null"
-}
-
-variable "Clinet_Secret"{ 
-    type = string
-    default = "null"
-}
-variable "Tenent_Id"{ 
-    type = string
-    default = "null"
-}
-variable "Subscription_Id"{ 
-    type = string
-    default = "null"
-}
