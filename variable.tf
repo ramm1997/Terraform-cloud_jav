092e1233a364fa49e6a46ed4f4b40ba6ac8ac3db
Sonushaik99 <116141459+Sonushaik99@users.noreply.github.com>
2022-10-19 17:41:27 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,14 +1,20 @@
 variable "subscription_id" {
+    default = ""
 }
 variable "tenant_id" {
+    default = ""
 }
 variable "client_id" {
+    default = ""
 }
 variable "client_secret" {
+    default = ""
 }
 variable "name"{
+    default = ""
 }
-variable "location" { 
+variable "location" {
+    default = ""
 }
 
 
