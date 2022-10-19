8445e74191646bd2003e56cfccd9e2575f8d8e37
Sonushaik99 <116141459+Sonushaik99@users.noreply.github.com>
2022-10-19 11:35:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=2.46.0"
     }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.4.1"
-    }
   }
 }
 
@@ -18,12 +14,3 @@ provider "azurerm" {
   features {}
 }
 
-
-provider "kubernetes" {
-  host                        = "${azurerm_kubernetes_cluster.example.kube_config.0.host}"
-  username                    = "${azurerm_kubernetes_cluster.example.kube_config.0.username}"
-  password                    = "${azurerm_kubernetes_cluster.example.kube_config.0.password}"
-  client_certificate        = "${base64decode(azurerm_kubernetes_cluster.example.kube_config.0.client_certificate)}"
-  client_key                = "${base64decode(azurerm_kubernetes_cluster.example.kube_config.0.client_key)}"
-  cluster_ca_certificate    = "${base64decode(azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)}"
-}
