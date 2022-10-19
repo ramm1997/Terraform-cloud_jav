48b935477bd229a9eed572b8d3fdc616676947c8
Sonushaik99 <116141459+Sonushaik99@users.noreply.github.com>
2022-10-19 17:51:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,19 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
+ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.10.0"
+      version = "=2.46.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.4.1"
     }
   }
-
-  required_version = ">= 1.2.3" {
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
