ef1ddfec24d215fc91487697362716948074c180
ramm1997 <80684861+ramm1997@users.noreply.github.com>
2022-10-26 12:06:39 +0530

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,11 +1,11 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "${var.name}-SA"
+  name                     = "${var.name}storage"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = {
-    owner = "javeed"
+    owner = "admin"
   }
 }
